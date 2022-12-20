30f4163818ce2589d8892c8d6b0c3a8983206c30
GianlucaMelis <gianluca.melis05@gmail.com>
2022-12-20 18:06:55 +0100

    restauro vecchio invio mail

--- form-to-email-casa.php
+++ form-to-email-casa.php
@@ -2,35 +2,7 @@
 <!--
 PHP mail send
 
-//variabili
-  $Luogo_di_ubicazione = $_POST['Luogo_di_ubicazione'];
-  $Tipo_Abitazione = $_POST['Tipo_Abitazione'];
-  $Valore = $_POST['Valore'];
-  $Tipo = $_POST['Tipo'];
-  $Garanzia = $_POST['Garanzia'];
-  $Email = $_POST['Email'];
-  $Telefono = $_POST['Telefono'];
-  $Eventuali_richieste = $_POST['Eventuali_richieste'];
 
- $ip = getenv(REMOTE_ADDR);
- $data = date("j F, Y, G:i:s T", time());
-//mail 
-    $email_from = 'rsyqlumu@preventivatoretestbm.x10.mx';
-	$email_subject = "NUOVA RICHIESTA";
-	$email_body = "
-    	Luogo di ubicazione: $Luogo_di_ubicazione\n
-        Tipo di abitazione: $Tipo_Abitazione \n
-        Valore: $Valore $Tipo\n  
-        Garanzia: $Garanzia\n
-        ---------------
-        E-mail: $Email\n
-        Telefono: $Telefono\n
-        Richieste aggiuntive: $Eventuali_richieste\n\n\n\n".               
-
-//invio        
-  $to = "infodati.bm@gmail.com";
-  $headers = "From: $email_from \r\n";
-  mail($to,$email_subject,$email_body,$headers);
 -->
 
 <!--
@@ -77,19 +49,6 @@ Luogo di ubicazione: $Luogo_di_ubicazione\n
   Richieste aggiuntive: $Eventuali_richieste\n\n\n\n";
 
 $mail->send()
-
-//per prendere gli indirizzi ip dei visitatori e salvarli in un file
-$filename = 'users.txt';
-$text = "\n".'Ip:'.$ip.'		Data:'.$data;
-
-$bytes = file_put_contents($filename, $text, FILE_APPEND);
-
-if (false === $bytes) {
-    exit;
-}
-
-
-
 -->
   <head>
 	<title> Preventivatore Online</title>
@@ -109,5 +68,44 @@ if (false === $bytes) {
   </body>
 </html>
 <?php
+//variabili
+$Luogo_di_ubicazione = $_POST['Luogo_di_ubicazione'];
+$Tipo_Abitazione = $_POST['Tipo_Abitazione'];
+$Valore = $_POST['Valore'];
+$Tipo = $_POST['Tipo'];
+$Garanzia = $_POST['Garanzia'];
+$Email = $_POST['Email'];
+$Telefono = $_POST['Telefono'];
+$Eventuali_richieste = $_POST['Eventuali_richieste'];
 
+$ip = getenv(REMOTE_ADDR);
+$data = date("j F, Y, G:i:s T", time());
+//mail 
+  $email_from = 'rsyqlumu@preventivatoretestbm.x10.mx';
+$email_subject = "NUOVA RICHIESTA";
+$email_body = "
+    Luogo di ubicazione: $Luogo_di_ubicazione\n
+      Tipo di abitazione: $Tipo_Abitazione \n
+      Valore: $Valore $Tipo\n  
+      Garanzia: $Garanzia\n
+      ---------------
+      E-mail: $Email\n
+      Telefono: $Telefono\n
+      Richieste aggiuntive: $Eventuali_richieste\n\n\n\n".               
+
+//invio        
+$to = "infodati.bm@gmail.com";
+$headers = "From: $email_from \r\n";
+mail($to,$email_subject,$email_body,$headers);
+
+
+//per prendere gli indirizzi ip dei visitatori e salvarli in un file
+$filename = 'users.txt';
+$text = "\n".'Ip:'.$ip.'		Data:'.$data;
+
+$bytes = file_put_contents($filename, $text, FILE_APPEND);
+
+if (false === $bytes) {
+    exit;
+}
  ?>
